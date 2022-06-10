eb6f13c8ce8f33444335124f98e7e89be7100c4f
skis630 <skis630@aucklanduni.ac.nz>
2022-06-10 11:04:15 +0300

    Prettier auto formatting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-    
-  const [ buttonColor, setButtonColor ] = useState('MediumVioletRed');
-  const [ disabled, toggleDisabled ] = useState(false);
-  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
-  
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+  const [disabled, toggleDisabled] = useState(false);
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
+
   return (
     <div>
-        <button
-            style={{backgroundColor: disabled ? 'gray' : buttonColor, color: 'white'}}
-            onClick={() => setButtonColor(newButtonColor)}
-            disabled={disabled}
-          >Change to {newButtonColor}</button>
-        <br />
-        <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={(e) => toggleDisabled(e.target.checked)}
-        />
+      <button
+        style={{
+          backgroundColor: disabled ? "gray" : buttonColor,
+          color: "white",
+        }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {newButtonColor}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        defaultChecked={false}
+        onChange={(e) => toggleDisabled(e.target.checked)}
+      />
     </div>
   );
 }
