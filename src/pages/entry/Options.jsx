bbb37827c0edfdbf533e79c1e80928f79e0185ea
skis630 <skis630@aucklanduni.ac.nz>
2022-06-12 22:40:16 +0300

    Error response from server code and tests

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -13,11 +15,12 @@ export default function Options({ optionType }) {
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        // TODO: handle error response
+        setError(true);
       });
   }, [optionType]);
 
-  // TODO: replace `null` with ToppingOption when available
+  if (error) return <AlertBanner />;
+
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
