867a10064be5fe3d1f456208b8c40df22c5af403
skis630 <skis630@aucklanduni.ac.nz>
2022-06-13 11:22:19 +0300

    Add context and wrap in provider

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import { useState } from "react";
-import "./App.css";
+import Container from "react-bootstrap/Container";
 import OrderEntry from "./pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
-  const [disabled, toggleDisabled] = useState(false);
-  const newButtonColor =
-    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
-
   return (
-    <div>
-      <OrderEntry />
-      <button
-        style={{
-          backgroundColor: disabled ? "gray" : buttonColor,
-          color: "white",
-        }}
-        onClick={() => setButtonColor(newButtonColor)}
-        disabled={disabled}
-      >
-        Change to {newButtonColor}
-      </button>
-      <br />
-      <input
-        type="checkbox"
-        defaultChecked={false}
-        onChange={(e) => toggleDisabled(e.target.checked)}
-      />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider */}
+    </Container>
   );
 }
 
