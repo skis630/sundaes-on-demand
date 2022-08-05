0a880c1ab7938cb983e71730069c757ad2be2ce4
skis630 <skis630@aucklanduni.ac.nz>
2022-08-05 18:53:51 +0300

    Disable order button if there are no scoops

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -5,13 +5,18 @@ import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function OrderEntry({ setOrderPhase }) {
   const [OrderDetails] = useOrderDetails();
+  const disabled = OrderDetails.totals.scoops === "$0.00";
 
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {OrderDetails.totals.grandTotal}</h2>
-      <Button variant="primary" onClick={() => setOrderPhase("review")}>
+      <Button
+        variant="primary"
+        onClick={() => setOrderPhase("review")}
+        disabled={disabled}
+      >
         Order
       </Button>
     </div>
