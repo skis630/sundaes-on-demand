c32c3825b648783d2923b0a11ff7eb604c401d69
skis630 <skis630@aucklanduni.ac.nz>
2022-08-05 18:53:36 +0300

    Test that order button is disabled if no scoops

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -3,6 +3,7 @@ import {
   screen,
   waitFor,
 } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -23,4 +24,38 @@ test("handles error for scoops and toppings routes", async () => {
     const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
   });
+});
+
+test("order button is disabled if no scoops are ordered", async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  // no scoops added
+  const orderButton = screen.getByRole("button", { name: /order/i });
+  expect(orderButton).toBeDisabled();
+
+  //   add 1 scoop of vanilla and 1 scoop of chocolate
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await userEvent.clear(vanillaInput);
+  await userEvent.type(vanillaInput, "1");
+  const chocolateInput = screen.getByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  await userEvent.clear(chocolateInput);
+  await userEvent.type(chocolateInput, "1");
+
+  const enabledOrderButton = screen.getByRole("button", { name: /order/i });
+  expect(enabledOrderButton).toBeEnabled();
+
+  // Remove scoops from order
+  const noVanilla = screen.getByRole("spinbutton", { name: "Vanilla" });
+  await userEvent.clear(noVanilla);
+  await userEvent.type(noVanilla, "0");
+  const noChocolate = screen.getByRole("spinbutton", { name: "Chocolate" });
+  await userEvent.clear(noChocolate);
+  await userEvent.type(noChocolate, "0");
+
+  const disabledOrderButton = screen.getByRole("button", { name: /order/i });
+  expect(disabledOrderButton).toBeDisabled();
 });
